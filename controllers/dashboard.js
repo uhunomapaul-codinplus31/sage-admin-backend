fa1e09db768b9d2c9405ba200230642154f6e99e
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-07 15:54:07 +0100

    d3d3df3

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,6 @@
 
 const db = require("../db/db")
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 // Login controller
 const dashIndex = async (req, res) => {
@@ -23,9 +23,9 @@ const dashIndex = async (req, res) => {
     // Compare passwords
     // Login successful
     res.status(200).json({
-      message: "Login successful",
+      
       user: user,
-      token: token
+    
     })
   } catch (error) {
     console.error("Login error:", error)
