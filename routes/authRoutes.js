4e31a31644be5c0fd9d1e799dfeaceae07a80052
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-07 16:42:33 +0100

    rh

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const dashController = require("../controllers/dashboard")
 const customerController = require("../controllers/customer")
 const inventoryController = require("../controllers/inventory")
 const paymentController = require("../controllers/payment")
+const loanController = require("../controllers/loan")
 
 
 // Check if controller functions exist before using them
@@ -40,9 +41,7 @@ router.get("/customer", (req, res, next) => {
 })
 router.get("/inventory/product", (req, res, next) => {
   try {
-    // invent_pd,
-    // invent_od,
-    // invent_rr
+  
     return inventoryController.invent_pd(req, res, next)
   } catch (error) {
     console.error("Error in login route:", error)
@@ -51,9 +50,7 @@ router.get("/inventory/product", (req, res, next) => {
 })
 router.get("/inventory/order", (req, res, next) => {
   try {
-    // invent_pd,
-    // invent_od,
-    // invent_rr
+
     return inventoryController.invent_od(req, res, next)
   } catch (error) {
     console.error("Error in login route:", error)
@@ -62,9 +59,7 @@ router.get("/inventory/order", (req, res, next) => {
 })
 router.get("/inventory/refund", (req, res, next) => {
   try {
-    // invent_pd,
-    // invent_od,
-    // invent_rr
+
     return inventoryController.invent_rr(req, res, next)
   } catch (error) {
     console.error("Error in login route:", error)
@@ -73,9 +68,7 @@ router.get("/inventory/refund", (req, res, next) => {
 })
 router.get("/payment/rg_transaction", (req, res, next) => {
   try {
-    // invent_pd,
-    // invent_od,
-    // invent_rr
+
     return paymentController.payment_rg(req, res, next)
   } catch (error) {
     console.error("Error in login route:", error)
@@ -84,10 +77,44 @@ router.get("/payment/rg_transaction", (req, res, next) => {
 })
 router.get("/payment/loan_transaction", (req, res, next) => {
   try {
-    // invent_pd,
-    // invent_od,
-    // invent_rr
+  
     return paymentController.payment_loan(req, res, next)
+  } catch (error) {
+    console.error("Error in login route:", error)
+    return res.status(500).json({ message: "Internal server error in login route" })
+  }
+})
+router.get("/loan/app/all", (req, res, next) => {
+  try {
+  
+    return loanController.loan_app_all(req, res, next)
+  } catch (error) {
+    console.error("Error in login route:", error)
+    return res.status(500).json({ message: "Internal server error in login route" })
+  }
+})
+router.get("/loan/repayment/overdue", (req, res, next) => {
+  try {
+  
+    return loanController.loan_rp_overdue_activities(req, res, next)
+  } catch (error) {
+    console.error("Error in login route:", error)
+    return res.status(500).json({ message: "Internal server error in login route" })
+  }
+})
+router.get("/loan/repayment/ongoing", (req, res, next) => {
+  try {
+  
+    return loanController.loan_rp_on_activities(req, res, next)
+  } catch (error) {
+    console.error("Error in login route:", error)
+    return res.status(500).json({ message: "Internal server error in login route" })
+  }
+})
+router.get("/loan/repayment/paidloan", (req, res, next) => {
+  try {
+  
+    return loanController.loan_rp_pd_activities(req, res, next)
   } catch (error) {
     console.error("Error in login route:", error)
     return res.status(500).json({ message: "Internal server error in login route" })
