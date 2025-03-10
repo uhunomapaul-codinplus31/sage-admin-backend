68261e5838fc5f70d381c66fcf859f5046910ce5
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-11 00:23:22 +0100

    3r3r3

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -5,8 +5,11 @@ const db = require("../db/db")
 // Login controller
 const invent_pd = async (req, res) => {
   try {
-    
-    const result = await db.query("SELECT * FROM inventory_pd_activities LIMIT 10")
+    // from chartcheckout get payment_status, total_shipping and  it should also include user  table name with the user id and include product table   dealer_id, display_photos, category, quantity_in_stock, name from the public_id and don't add cc.items amd do not use jsonb_array_elements(json)
+    const result = await db.query(`SELECT cc.payment_status, cc.total_shipping, u.id AS user_id, u.first_name, u.last_name, p.dealer_id, p.display_photos, p.category, p.quantity_in_stock, p.name
+    FROM cartcheckout cc
+    JOIN "user" u ON cc.user_id = u.id
+    JOIN product p ON p.public_id = cc.public_id`)
     const record = result.rows[0]
 
     
