8d6aad345927c5be4c1f3704d5945b90e271d6f4
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-11 11:41:48 +0100

    fbnfnf

--- controllers/customer.js
+++ controllers/customer.js
@@ -7,12 +7,21 @@ const cusIndex = async (req, res) => {
   try {
     
     const result = await db.query('SELECT u.phone_number, u.email, u.first_name, u.last_name, u.is_phone_number_verified, u.uid, u.id, u.paystack_customer_id, up.is_verified FROM public."user" u LEFT JOIN public.userprofile up ON u.id = up.user_id')
+
+    const allusers = await db.query('SELECT COUNT(*) FROM "user"')
     const record = result.rows
+    const applicant = await db.query('SELECT COUNT(*) FROM userprofile WHERE is_verified = false')
+    const await_verification = applicant.rows[0].count
+    const alluser = allusers.rows[0].count
 
     
     res.status(200).json({
       
       record: record,
+      customer: alluser,
+      await_verification: await_verification,
+      total_applicant: 0,
+      pending_application: 0
     
     })
   } catch (error) {
