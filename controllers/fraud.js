887b5c154967c6d93042da1e165a5b818d2aea08
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-20 19:26:22 +0100

    Update fraud.js

--- controllers/fraud.js
+++ controllers/fraud.js
@@ -19,6 +19,25 @@ const fraudIndex = async (req, res) => {
   }
 }
 
+const fraudIndex = async (req, res) => {
+  try {
+    
+    const result = await db.query("SELECT * FROM flagged_user")
+    const record = result.rows
+
+    
+    res.status(200).json({
+      
+      record: record,
+    
+    })
+  } catch (error) {
+    console.error("Login error:", error)
+    res.status(500).json({ message: "Server error", error: error.message })
+  }
+}
+
+
 // Register controller
 module.exports = {
     fraudIndex
