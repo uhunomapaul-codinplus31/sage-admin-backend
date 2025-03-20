5b2411ade5b65f1a59f34b9020492ed81ce5e801
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-20 19:38:16 +0100

    Update customer.js

--- controllers/customer.js
+++ controllers/customer.js
@@ -77,8 +77,40 @@ AND cc.user_id = $1`,[id])
   }
 }
 
+
+const payment_method = async (req, res) => {
+  try {
+    let {id} = req.query;
+
+//     SELECT cc.payment_status, cc.total_shipping, u.id AS user_id, u.first_name, u.last_name,
+//     p.dealer_id, p.display_photos, p.category, p.quantity_in_stock, p.name
+// FROM cartcheckout cc
+// JOIN public."user" u ON cc.user_id = u.id
+// LEFT JOIN LATERAL jsonb_array_elements(cc.items::jsonb) AS item ON true
+// LEFT JOIN public."product" p ON p.id = (item->>'product_id')::bigint
+// WHERE cc.items IS NOT NULL
+
+
+
+    const result = await db.query(`SELECT * FROM credittransaction WHERE account_id = $1`,[id])
+
+    
+    const record = result.rows
+    
+
+    
+    res.status(200).json({
+      record: record,
+    })
+  } catch (error) {
+    console.error("Login error:", error)
+    res.status(500).json({ message: "Server error", error: error.message })
+  }
+}
+      
 // Register controller
 module.exports = {
     cusIndex,
-    orderdetails
+    orderdetails,
+  payment_method
 }
